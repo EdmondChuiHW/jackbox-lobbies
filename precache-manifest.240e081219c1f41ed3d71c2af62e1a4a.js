e9cbbbfa0ae3067848359d1f654de7b13610e75f
Edmond Chui <edmond@MBP.local>
2020-05-16 21:37:24 -0600

    Updates

---
+++ precache-manifest.240e081219c1f41ed3d71c2af62e1a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df078382cbdae595b8253ed0215cde3",
+    "url": "/jackbox-lobbies/index.html"
+  },
+  {
+    "revision": "4a0647db001d989f774d",
+    "url": "/jackbox-lobbies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c875831a024205bf7953",
+    "url": "/jackbox-lobbies/static/js/2.04985918.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jackbox-lobbies/static/js/2.04985918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0647db001d989f774d",
+    "url": "/jackbox-lobbies/static/js/main.87ebc43c.chunk.js"
+  },
+  {
+    "revision": "321e106e669b924081b3",
+    "url": "/jackbox-lobbies/static/js/runtime-main.3c4083ae.js"
+  }
+]);
